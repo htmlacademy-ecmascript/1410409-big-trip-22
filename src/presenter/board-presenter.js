5f51c43ac75735c8522666686214422b87ad5b9e
denispan <6231990@gmail.com>
2023-12-08 16:43:02 +0100

    Добавляет константу

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,6 +4,7 @@ import {render} from '../render';
 import SortView from '../view/sort-view';
 import EventEditView from '../view/event-edit-view';
 import EventView from '../view/event-view';
+import {EVENT_COUNT} from '../constants';
 
 export default class BoardPresenter {
   boardComponent = new BoardView();
@@ -19,7 +20,7 @@ export default class BoardPresenter {
     render(this.eventListComponent, this.boardComponent.getElement());
     render(new EventEditView(), this.eventListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < EVENT_COUNT; i++) {
       render(new EventView(), this.eventListComponent.getElement());
     }
   }
