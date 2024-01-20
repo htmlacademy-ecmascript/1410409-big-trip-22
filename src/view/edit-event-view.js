2b10c5275ad6bfd2f1292263b916dce0574012a0
denispan <6231990@gmail.com>
2024-01-20 10:39:26 +0100

    добавляет id

--- src/view/edit-event-view.js
+++ src/view/edit-event-view.js
@@ -9,8 +9,8 @@ function createEventTypeListTemplate(availableTypes) {
     <legend class="visually-hidden">Event type</legend>
 
     ${availableTypes.map((type) => `<div class="event__type-item">
-      <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value=${type}>
-      <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${capitalizeFirstLetter(type)}</label>
+      <input id="event-type-${type}" class="event__type-input  visually-hidden" type="radio" name="event-type" value=${type}>
+      <label class="event__type-label  event__type-label--${type}" for="event-type-${type}">${capitalizeFirstLetter(type)}</label>
     </div>`).join('')}
 
   </fieldset>`;
@@ -78,6 +78,7 @@ function createDestinationTemplate(destinationData) {
 
 function createEditEventTemplate(event, allOffers, allDestinations) {
   const {
+    id,
     dateFrom,
     dateTo,
     type,
@@ -91,11 +92,11 @@ function createEditEventTemplate(event, allOffers, allDestinations) {
     `<form class="trip-events__item event event--edit" action="#" method="post">
       <header class="event__header">
         <div class="event__type-wrapper">
-          <label class="event__type  event__type-btn" for="event-type-toggle-1">
+          <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
             <span class="visually-hidden">Choose event type</span>
             <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
           </label>
-          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-${id}" type="checkbox">
 
           <div class="event__type-list">
             ${createEventTypeListTemplate(EVENT_TYPES)}
@@ -103,11 +104,11 @@ function createEditEventTemplate(event, allOffers, allDestinations) {
         </div>
 
         <div class="event__field-group  event__field-group--destination">
-          <label class="event__label  event__type-output" for="event-destination-1">
+          <label class="event__label  event__type-output" for="event-destination-${destinationData.id}">
             ${capitalizeFirstLetter(type)}
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value=${destinationData.name} list="destination-list-1">
-          <datalist id="destination-list-1">
+          <input class="event__input  event__input--destination" id="event-destination-${destinationData.id}" type="text" name="event-destination" value=${destinationData.name} list="destination-list-1">
+          <datalist id="destination-list-${destinationData.id}">
             <option value="Amsterdam"></option>
             <option value="Geneva"></option>
             <option value="Chamonix"></option>
@@ -115,10 +116,10 @@ function createEditEventTemplate(event, allOffers, allDestinations) {
         </div>
 
         <div class="event__field-group  event__field-group--time">
-          <label class="visually-hidden" for="event-start-time-1">From</label>
+          <label class="visually-hidden" for="event-start-time-${id}">From</label>
           <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value=${dayjs(dateFrom).format(DATE_FORMAT_INPUT_DATE)}&nbsp;${dayjs(dateFrom).format(DATE_FORMAT_INPUT_TIME)}>
           &mdash;
-          <label class="visually-hidden" for="event-end-time-1">To</label>
+          <label class="visually-hidden" for="event-end-time-${id}">To</label>
           <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value=${dayjs(dateTo).format(DATE_FORMAT_INPUT_DATE)}&nbsp;${dayjs(dateTo).format(DATE_FORMAT_INPUT_TIME)}>
         </div>
 
